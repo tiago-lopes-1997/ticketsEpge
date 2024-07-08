aea5838fb97c93dfff83d7ae905878bb67423c5f
tiago-lopes-1997 <tjhl644@gmail.com>
2024-07-08 15:19:19 +0100

    Stable Login

--- server/server.js
+++ server/server.js
@@ -1,27 +1,21 @@
 const express = require('express');
-const app = express();
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
+const app = express();
 const port = 5000;
-
 
 app.use(bodyParser.json());
 
-//api test
-app.get("/api", (req, res) => {
-    res.json({ "users": ["userOne", "userTwo", "userThree"] })
-})
+const usersFilePath = path.join(__dirname, '../dbTest/users.txt');
 
-const usersFilePath = '../dbTest/users.txt';
-
-//Login API
+// Login API
 app.post("/api/login", (req, res) => {
     const { username, password } = req.body;
-    //const filePath = path.join(__dirname, 'dbTest/users.txt');
+    console.log(`Login attempt: ${username}`); // Add logging here
 
     fs.readFile(usersFilePath, { encoding: 'utf-8'}, (err, data) => {
-        if(err){
+        if (err) {
             console.error(err);
             return res.status(500).send('Error reading file');
         }
@@ -29,14 +23,16 @@ app.post("/api/login", (req, res) => {
         const users = data.split('\n').map(line => line.split('#'));
         const user = users.find(u => u[0] === username && u[1] === password);
 
-        if(user){
-            const userType = user[2];
+        if (user) {
+            const userType = user[2].trim();
+            console.log(`User authenticated: ${username}, Type: ${userType}`); // Add logging here
             return res.json({ userType });
-        }else{
-            res.status(401).json({ sucess: false, message: 'Invalid Credencitials'});
+        } else {
+            res.status(401).json({ success: false, message: 'Invalid Credentials' });
         }
     });
 });
 
-app.listen(port, () => {console.log("Server is running on http://localhost:${port}")})
- 
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
